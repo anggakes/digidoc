07db3ec721ea0bbe248a84bdca70ecef30543742
angga kesuma <anggakesuma@gmail.com>
2021-08-02 10:08:34 +0700

    test notification

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -16,4 +16,14 @@
 
 @section('js')
 <script> console.log('Hi!'); </script>
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+        OneSignal.init({
+            appId: "d4de6a7c-fb5b-4b11-b0d0-dfa7c6265306",
+        });
+        OneSignal.setExternalUserId("{{ Auth::user()->id }}");
+    });
+</script>
 @stop
