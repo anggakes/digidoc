4ea7c332ea504c248b4b7900e626a0ba33448aea
angga kesuma <anggakesuma@gmail.com>
2021-08-30 18:03:48 +0700

    delete menu surat keluar

--- example-app/resources/views/document/compose.blade.php
+++ example-app/resources/views/document/compose.blade.php
@@ -48,18 +48,7 @@
             <!-- /.info-box-content -->
         </a>
     </div>
-    <div class="col-sm-4 col-xs-12">
-        <a href='{{ route("document.suratKeluar") }}' class="info-box">
 
-            <!-- Apply any bg-* class to to the icon to color it -->
-            <span class="info-box-icon bg-purple"><i class="fa fa-envelope-square"></i></span>
-            <div class="info-box-content">
-                <span class="info-box-text info-box-title">Buat Surat Keluar</span>
-                <span>Surat untuk instansi lain atau cabang lainnya</span>
-            </div>
-            <!-- /.info-box-content -->
-        </a>
-    </div>
 
     <div class="col-sm-4 col-xs-12">
         <a href='{{ route("document.suratMasuk") }}' class="info-box">
